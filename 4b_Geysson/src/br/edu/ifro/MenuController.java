a001150f556552672d90b10add12ed46e85bc87b
Geysson <geyssongg@gmail.com>
2018-05-04 15:06:15 -0400

    Instalando o hibernate

--- 4b_Geysson/src/br/edu/ifro/MenuController.java
+++ 4b_Geysson/src/br/edu/ifro/MenuController.java
@@ -10,7 +10,9 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -20,12 +22,47 @@ public class MenuController implements Initializable {
     
     @FXML
     private Label label;
+    @FXML
+    private TextField  txtResultado; 
+    @FXML
+    private TextField txtNumero2;
+    @FXML 
+    private TextField txtNumero1;
+    @FXML
+    private Button btnSoma,btnMenos,btnVezes,btnDividir;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+   private void soma (){
+       Double num1 = Double.parseDouble(txtNumero1.getText());
+       Double num2 = Double.parseDouble(txtNumero2.getText());
+       Double resultado = num1+num2;
+       
+       txtResultado.setText(resultado.toString());
+   }
+   @FXML
+   private void menos(){
+          Double num1 = Double.parseDouble(txtNumero1.getText());
+       Double num2 = Double.parseDouble(txtNumero2.getText());
+       Double resultado = num1-num2;
+       
+       txtResultado.setText(resultado.toString());
+   }
+   @FXML
+   private void vezes (){
+          Double num1 = Double.parseDouble(txtNumero1.getText());
+       Double num2 = Double.parseDouble(txtNumero2.getText());
+       Double resultado = num1*num2;
+       
+       txtResultado.setText(resultado.toString());
+   }
+   @FXML
+   private void dividir (){
+          Double num1 = Double.parseDouble(txtNumero1.getText());
+       Double num2 = Double.parseDouble(txtNumero2.getText());
+       Double resultado = num1/num2;
+       
+       txtResultado.setText(resultado.toString());
+   }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
